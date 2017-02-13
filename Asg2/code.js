625f8b4e2ca710adfe0c7c290b733a6da6a5cd24
Manorie Vachon <mvach854@mtroyal.ca>
2017-02-12 21:52:16 -0700

    More functions ya :)

--- Asg2/code.js
+++ Asg2/code.js
@@ -4,12 +4,12 @@
 function getStats(txt) {
     return {
         nChars: txt.length,
-        nWords: calcNWords(txt),
+        nWords: calcNWords(txt).length,
         nLines: calcNLines(txt),
         nNonEmptyLines: calcNNonEmptyLines(txt),
-     //  averageWordLength: 3.3,
-     //  maxLineLength: 33,
-     //  palindromes: ["12321", "kayak", "mom"],
+        averageWordLength: calcAverageWordLength(txt),
+        maxLineLength: calcMaxLineLength(txt),
+        palindromes: findPalindromes(txt),
      //  longestWords: ["xxxxxxxxx", "123444444"],
      //  mostFrequentWords: ["hello(7)", "world(1)"]
     };
@@ -19,7 +19,7 @@ function getStats(txt) {
 function calcNWords(txt) {
 	let alphaNum = txt.replace(/[^a-z0-9]/ig, " ");
 	let splitWords = alphaNum.split(/\s+/).filter(Boolean); // get rid of empty string entries
-	return splitWords.length;
+	return splitWords;
 }
 
 // nLines
@@ -35,11 +35,51 @@ function calcNLines(txt) {
 
 function calcNNonEmptyLines(txt) {
 	let nLines = txt.split("\n");
-		console.log(nLines);
-
 	let nNonEmptyLines = nLines.filter(function(arr) {
 		return /\S/.test(arr);
 	});
-	console.log(nNonEmptyLines);
 	return nNonEmptyLines.length;
+}
+
+function calcAverageWordLength(txt) {
+	let words = calcNWords(txt);
+	let totalLength = 0;
+	for (let i = 0; i < words.length; i++) {
+		totalLength += words[i].length;
+	}
+	return totalLength / words.length;
+}
+
+function calcMaxLineLength(txt) {
+	let lines = txt.split("\n");
+	let longest = 0;
+	
+	for (let i = 0; i < lines.length; i++) {
+		if (lines[i].length > longest) {
+			longest = lines[i].length;
+		}
+	}
+	return longest;
+}
+
+function findPalindromes(txt) {
+	let words = calcNWords(txt);
+	let palindromes = [];
+	
+	for (let i = 0; i < words.length; i++) {
+		words[i] = words[i].toLowerCase();
+	}
+	let wordsOver2 = words.filter(function(word) { // remove any words 2 characters or less
+		return word.length > 2;
+	});
+	let uniqueWordsOver2 = wordsOver2.filter(function(word, i) { // remove any duplicates
+		return wordsOver2.indexOf(word) === i;
+	});
+	for (let i = 0; i < uniqueWordsOver2.length; i++) {
+		let reversedWord = uniqueWordsOver2[i].split("").reverse().join("");
+		if (uniqueWordsOver2[i] === reversedWord) {
+			palindromes.push(uniqueWordsOver2[i]);
+		}
+	}
+	return palindromes;
 }
